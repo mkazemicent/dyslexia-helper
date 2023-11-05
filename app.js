ee9752503831d8ad7f49e65e95db3b0439da6d52
mkazemicent <117585679+mkazemicent@users.noreply.github.com>
2023-11-05 18:54:19 -0500

    Setup initial server and view template

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
