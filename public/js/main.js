0b8dc933727f8fc6a073766312037cb642c0590f
mkazemicent <117585679+mkazemicent@users.noreply.github.com>
2023-11-05 22:18:37 -0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -3,30 +3,30 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Check for browser support
 const synth = window.speechSynthesis;
-// document.getElementById('speak').addEventListener('click', function () {
-//     if (synth) {
-//         const utterance = new SpeechSynthesisUtterance(document.getElementById('word').textContent);
-//         synth.speak(utterance);
-//     } else {
-//         alert("Sorry, your browser does not support text-to-speech!");
-//     }
-// });
 document.getElementById('speak').addEventListener('click', function () {
-    const text = document.getElementById('word').textContent;
-    fetch('/synthesize-speech', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ text: text }),
-    })
-    .then(response => response.blob())
-    .then(blob => {
-        const audioURL = window.URL.createObjectURL(blob);
-        new Audio(audioURL).play();
-    })
-    .catch(error => console.error('Error:', error));
+    if (synth) {
+        const utterance = new SpeechSynthesisUtterance(document.getElementById('word').textContent);
+        synth.speak(utterance);
+    } else {
+        alert("Sorry, your browser does not support text-to-speech!");
+    }
 });
+// document.getElementById('speak').addEventListener('click', function () {
+//     const text = document.getElementById('word').textContent;
+//     fetch('/synthesize-speech', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ text: text }),
+//     })
+//     .then(response => response.blob())
+//     .then(blob => {
+//         const audioURL = window.URL.createObjectURL(blob);
+//         new Audio(audioURL).play();
+//     })
+//     .catch(error => console.error('Error:', error));
+// });
 
 document.getElementById('check').addEventListener('click', function () {
     const correctSpelling = document.getElementById('word').textContent;
