ae0deb555371407ba75da34e14996d74522a371f
mkazemicent <117585679+mkazemicent@users.noreply.github.com>
2023-11-05 19:13:17 -0500

    Project Directory Structure

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// View Engine Setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.render('index');
+// Static Files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routing
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// Error Handler
+app.use((err, req, res, next) => {
+  // Set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // Render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
